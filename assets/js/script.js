706472bce040ca6dd75e48fe437c078b41e3b91a
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-01-12 15:27:25 +0000

    add function to calculate and check correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type")=== "submit"){
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                     runGame(gameType);
@@ -35,9 +35,37 @@ function runGame(gameType){
 
 function checkAnswer(){
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+     if (isCorrect){
+         alert("Hey! You got it right! :D");
+
+     } else {
+         alert (`aww.. you answered ${userAnswer}. the correct answer was ${calculatedAnswer[0]}!`)
+     }
+
+     runGame(calculatedAnswer[1]);
+
 }
 
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator==="+") {
+        return [operand1 + operand2, "addition"];
+
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+
+    
+
+    
 
 }
 
@@ -56,14 +84,16 @@ function displayAdditionQuestion (operand1, operand2){
 
 }
 
-function displaySubtractQuestion (){
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";
+function displaySubtractQuestion (operand1, operand2){
+    
 
 }
 
 function displayMultiplyQuestion (operand1, operand2){
 
+
+}
+
+function displayDivisionQuestion (operand1, operand2){
 
 }
