0f9bed008ee50e528e4c93e0a96495c67d10d8d1
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-01-12 15:07:58 +0000

    add display addition question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,31 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                    alert(`You clicked ${gameType}`);
+                    runGame(gameType);
                 }
         })
     }
+
+    runGame("addition");
 })
 
 
-function runGame(){
+function runGame(gameType){
 
-    Math.floor(Math.random() *25) + 1;
+    let num1 = Math.floor(Math.random() *25) + 1;
 
+    let num2 = Math.floor(Math.random() *25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}.aborting!`;
+
+    }
 }
+
+
 
 function checkAnswer(){
 
@@ -36,14 +49,21 @@ function incrementWrongAnswer (){
 
 }
 
-function displayAdditionQuestion (){
+function displayAdditionQuestion (operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
 function displaySubtractQuestion (){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
 
-function displayMultiplyQuestion (){
+function displayMultiplyQuestion (operand1, operand2){
+
 
 }
