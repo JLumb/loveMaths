b97f6696c4f8ec12ecb01315d0163560180b1431
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-01-12 16:05:54 +0000

    add quality of life and clean up eventlisteners

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,22 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key ==="Enter"){
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
-    runGame("multiply");
-    runGame("subtract");
+  
     
 })
 
 
 function runGame(gameType){
+
+    document.getElementById('answer-box').value="";
+    document.getElementById("answer-box").focus();
 
     let num1 = Math.floor(Math.random() *25) + 1;
 
